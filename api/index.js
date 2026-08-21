b6c61703805599ac9a9056252d150627c08ae23a
Hz00101 <hiarles.silva@aluno.educa.go.gov.br>
2026-08-21 10:26:15 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -146,9 +146,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-// START
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
